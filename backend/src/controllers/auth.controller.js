2a53bf9338dfb6a5158d0f5768d7d0bd5efd90ab
BenArbiaAhmed <hamoudabenarbia15@gmail.com>
2025-07-12 22:31:56 +0100

    front and back dependencies + auth routes

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req,res)=> {
+    res.send("Sign up request");
+}
+
+export const login = (req,res)=> {
+    console.log("login request")
+}
+
+export const logout = (req,res)=> {
+    console.log("logout request")
+}
