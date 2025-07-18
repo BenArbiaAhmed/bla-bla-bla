c292ce8dc17059c8ea52bd0fdcb59ced9dad6352
BenArbiaAhmed <hamoudabenarbia15@gmail.com>
2025-07-18 22:51:05 +0100

    message model and routes

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,58 @@
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
+export const getUsersForSidebar = async (req, res) => {
+    try{
+        const loggedInUser = req.user._id;
+        const filteredUser = await User.find({_id: {$ne: loggedInUser}}).select("-password")
+        res.status(200).json(filteredUser);
+    }
+    catch(error){
+        console.log("Error in getUsersForSidebar", error);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id:userToChatId } = req.params;
+        const myId = req.user._id;
+        const messages = Message.find({
+            $or:[
+                {senderId: userToChatId, receiverId: myId},
+                {senderId: myId, receiverId: userToChatId}
+            ]
+        })
+        res.status(200).json(messages);
+    }
+    catch(error){
+        console.log("Error in getMessages", error);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try{
+        const { text, image } = req.body;
+        const { id:receiverId } = req.params;
+        const senderId = req.user._id;
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = new Message({
+            text,
+            image: imageUrl,
+            receiverId,
+            senderId
+        })
+        await newMessage.save();
+        res.status(201).json(newMessage);
+    }
+    catch(error){
+        console.log("Error in sendMessage", error);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
