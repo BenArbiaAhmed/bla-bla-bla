ca4fb3bd0b8e23594cc6a8ff50c39a77cc09b619
BenArbiaAhmed <hamoudabenarbia15@gmail.com>
2025-07-20 23:05:44 +0100

    worked on frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,15 +4,21 @@ import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv';
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5181",
+    credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
+
 
 const port = process.env.PORT;
 app.listen(port, (req, res) => {
--- backend/src/middleware/authh.middleware.js
+++ backend/src/middleware/authh.middleware.js
@@ -6,7 +6,7 @@ export const protectRoute = async (req, res, next) => {
     try{
         const token = req.cookies.jwt
         if(!token){
-            return res.status(401).json("Unauthorized - No token provided");
+            return res.status(401).json({message: "Unauthorized - No token provided"});
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         if(!decoded){
